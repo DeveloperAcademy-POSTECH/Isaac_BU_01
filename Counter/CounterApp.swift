8180c604bcd95a7d13f8bdaf9feb26401f5d3ab0
Kang Hee Jong <olddeapo@gmail.com>
2022-05-23 13:23:18 +0900

    Initial Commit

---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by devisaac on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
